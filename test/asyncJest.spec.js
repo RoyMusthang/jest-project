bad8440220f579bf11291e596e593a57e987d445
Josue Lobo <darksiders20005@gmail.com>
2021-07-16 14:58:20 -0300

    requisito 1 ok

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -10,10 +10,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', async () => {
-    await expect(answerPhone(true)).resolves.toBe('Oi!');
+  test('atende', async () => { // async para deixar assincrono.
+    await expect(answerPhone(true)).resolves.toBe('Oi!'); // await para esperar algo ser resolvido, chama a função 
+    // com parametro true .resolves para pegar o acerto e comparar com a resposta esperada
   });
   test('ocupado', async () => {
     await expect(answerPhone(false)).rejects.toThrow(Error('Infelizmente não podemos atender...'));
+    // https://jestjs.io/pt-BR/docs/expect#tothrowerror toThrow para injetar error
   });
 });
